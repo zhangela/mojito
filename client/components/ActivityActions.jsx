6730d0083d9481127819cc06d468d484de6516ff
Angela Zhang <zhangela@mit.edu>
2016-01-01 19:02:31 -0800

    activity actions styling + save query to url
    fixes #1

--- client/components/ActivityActions.jsx
+++ client/components/ActivityActions.jsx
@@ -10,13 +10,19 @@ ActivityActions = React.createClass({
     const numPeople = this.getQuoteQuery("numPeople");
     let duration = FlowRouter.getQueryParam("duration") || null;
 
+    let quote = null;
+
     if (duration && duration !== "unselected") {
       duration = parseInt(duration, 10);
       const perPerson = _.findWhere(this.props.activity.pricing, {duration: duration}).perPerson;
-      const quote = numPeople * perPerson * (1 + this.props.activity.percentGratuity / 100);
-      return {quote: quote}
+      quote = numPeople * perPerson * (1 + this.props.activity.percentGratuity / 100);
     }
-    return {quote: null};
+
+    return {
+      quote: quote,
+      duration: duration,
+      numPeople: numPeople
+    };
   },
 
   getQuoteQuery(key) {
@@ -34,7 +40,6 @@ ActivityActions = React.createClass({
   },
 
   handleChange(event) {
-    console.log(event.target.value);
     FlowRouter.withReplaceState(function() {
       FlowRouter.setQueryParams({
           [event.target.name]: event.target.value,
@@ -47,22 +52,65 @@ ActivityActions = React.createClass({
     const activity = this.props.activity;
 
     return (
-      <div className="activityActions">
-          <input type="text" name="numPeople" value={this.getQuoteQuery('numPeople') || null} placeholder="# People" onChange={this.handleChange} />
-          <select name="duration" onChange={this.handleChange}>
-            <option value="unselected" key="unselected">Select one</option>
-            {activity.getSortedPricing().map((pricingObj, i) => {
-              return (
-                <option key={pricingObj.duration} value={pricingObj.duration}>
-                  {pricingObj.duration} {this.getHourUnit(pricingObj.duration)}: ${pricingObj.perPerson}
-                </option>
-              );
-            })}
-          </select>
-          <div>Gratuity: {activity.percentGratuity}%</div>
-          <div>
-            {this.data.quote}
-          </div>
+      <div className="ActivityActions">
+        <div className="QuoteBox">
+          <InputRow label="Number of people">
+            <input
+              className="numPeople form-control"
+              type="text"
+              name="numPeople"
+              value={this.data.numPeople || null}
+              placeholder="# People"
+              onChange={this.handleChange} />
+          </InputRow>
+
+          <InputRow label="Duration">
+            <select
+                className="duration form-control"
+                name="duration"
+                onChange={this.handleChange}>
+              <option value="unselected" key="unselected">Select one</option>
+
+              {activity.getSortedPricing().map((pricingObj, i) => {
+                return (
+                  <option
+                      key={pricingObj.duration}
+                      value={pricingObj.duration}
+                      selected={pricingObj.duration === this.data.duration}>
+                    {pricingObj.duration} {this.getHourUnit(pricingObj.duration)}: ${pricingObj.perPerson}/person
+                  </option>
+                );
+              })}
+            </select>
+          </InputRow>
+
+          <InputRow label="Gratuity">
+            <div className="gratuity">
+              {activity.percentGratuity}%
+            </div>
+          </InputRow>
+
+          <hr />
+
+          <InputRow label="Quote">
+            <div className="quote">
+              ${this.data.quote.toFixed(2) || "N/A"}
+            </div>
+          </InputRow>
+        </div>
+
+        <button className="contactHost btn btn-primary btn-lg btn-block">Contact Host</button>
+      </div>
+    );
+  }
+});
+
+InputRow = React.createClass({
+  render() {
+    return (
+      <div className="row InputRow">
+        <div className="col-xs-5 inputRowLabel">{this.props.label}</div>
+        <div className="col-xs-7">{this.props.children}</div>
       </div>
     );
   }
